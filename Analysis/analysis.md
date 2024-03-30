e62a7bdcc5a4f1d69d6d4d7f579a292ca18b30d4
Yatri Patel <yatripatel.1609@gmail.com>
2024-03-30 00:27:12 -0400

    Added more models and analysis

--- Analysis/analysis.md
+++ Analysis/analysis.md
@@ -26,3 +26,49 @@ The strategic use of "Period.Start" and "Period.End" caters to different analyti
 - **Decision-making:** Different stakeholders may require information specific to the beginning or end of risk periods. For example, emergency services might be more interested in "Period.Start" for immediate response planning, while long-term urban or community planning might focus more on "Period.End" to understand future risk landscapes.
 
 In summary, the choice between "Period.Start" and "Period.End" hinges on the specific objectives of the analysis, the nature of the decision-making process it informs, and the temporal insights required to effectively manage or mitigate asteroid impact risks.
+
+Results of models without any feature engineered data:
+
+                                      MSE            R2
+Linear Regression            2.149896e+03  2.640356e-01
+KNN                          2.380218e+03  1.851905e-01
+Stochastic Gradient Descent  5.908724e+21 -2.022708e+18
+DecisionTreeClassifier       6.388195e+03 -1.186843e+00
+Random Forest                1.824739e+03  3.753451e-01
+Gradient Boosting            2.176319e+03  2.549903e-01
+Ridge Regression             2.149861e+03  2.640474e-01
+
+Results of Poly Scaled data:
+
+                                      MSE            R2
+Linear Regression            4.167147e+03 -4.265213e-01
+KNN                          2.579562e+03  1.169498e-01
+Stochastic Gradient Descent  2.166098e+11 -7.415107e+07
+DecisionTreeClassifier       2.298213e+03  2.132629e-01
+Random Forest                2.372833e+03  1.877185e-01
+Gradient Boosting            3.488175e+03 -1.940915e-01
+Ridge Regression             2.297630e+03  2.134622e-01
+
+Results of RFE selected data:
+
+                                      MSE            R2
+Linear Regression            2.325755e+03  2.038346e-01
+KNN                          3.512773e+03 -2.025123e-01
+Stochastic Gradient Descent  1.001282e+17 -3.427646e+13
+DecisionTreeClassifier       8.232878e+03 -1.818325e+00
+Random Forest                2.503413e+03  1.430177e-01
+Gradient Boosting            2.314810e+03  2.075811e-01
+Ridge Regression             2.325714e+03  2.038484e-01
+
+
+Results of Scaled data:
+
+                                     MSE        R2
+Linear Regression            2149.895640  0.264036
+KNN                          2286.995102  0.217103
+Stochastic Gradient Descent  2150.063564  0.263978
+DecisionTreeClassifier       2049.128280  0.298531
+Random Forest                1887.934643  0.353712
+Gradient Boosting            2201.576138  0.246344
+Ridge Regression             2149.861321  0.264047
+
--- Code/asteroidImpact.py
+++ Code/asteroidImpact.py
@@ -3,13 +3,16 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import sklearn.model_selection
-from sklearn.linear_model import LogisticRegression, Ridge, LinearRegression
+from sklearn.linear_model import Ridge, LinearRegression, SGDRegressor
 from sklearn.metrics import *
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.feature_selection import RFE
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as pyplt
+from sklearn.tree import DecisionTreeClassifier
 
 # Load the dataset
 file_path = '../Data/processed_impacts.csv'
@@ -40,11 +43,14 @@ missing_values_after = impact_data_cleaned.isnull().sum()
 print(missing_values_after)
 
 # Plot correlation heatmap for the impact data
-sns.heatmap(impact_data_cleaned.corr(), cmap="YlGnBu", annot=True)
+colormap = pyplt.cm.viridis
+sns.heatmap(impact_data_cleaned.corr(), cmap=colormap, annot=True)
 plt.show()
 
 # Drop Period Start and Period End due to multi-collinearity as Period is already in the data
-impact_data_cleaned = impact_data_cleaned.drop(columns=['Period Start','Period End'])
+impact_data_cleaned = impact_data_cleaned.drop(columns=['Period Start', 'Period End'])
+# Drop Torino Scale and Palermo scale max due to multi-collinearity as well
+impact_data_cleaned = impact_data_cleaned.drop(columns=['Torino.Scale..max..', 'Palermo.Scale..max..'])
 
 # Define the features (X) and the target (y)
 X = impact_data_cleaned.drop(columns=['Potential.Impacts..'])
@@ -53,22 +59,35 @@ y = impact_data_cleaned['Potential.Impacts..']
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2, random_state=42)
 
-print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+# Define models to test
+models = {
+    "Linear Regression": LinearRegression(),
+    "KNN": KNeighborsRegressor(),
+    "Stochastic Gradient Descent": SGDRegressor(),
+    "DecisionTreeClassifier": DecisionTreeClassifier(),
+    "Random Forest": RandomForestRegressor(random_state=42),
+    "Gradient Boosting": GradientBoostingRegressor(random_state=42),
+    "Ridge Regression": make_pipeline(StandardScaler(), Ridge(random_state=42))
+}
 
-# Initialize the Linear Regression model
-lr_model = sklearn.linear_model.LinearRegression()
+# Initialize dictionary to store results
+results = {}
 
-# Train the model
-lr_model.fit(X_train, y_train)
+# Train and evaluate each model without any feature engineering
+for name, model in models.items():
+    # Train the model
+    model.fit(X_train, y_train)
+    # Predict on the testing set
+    y_pred = model.predict(X_test)
+    # Calculate metrics
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    # Store results
+    results[name] = {'MSE': mse, 'R2': r2}
 
-# Predict on the testing set
-y_pred = lr_model.predict(X_test)
-
-# Evaluate the model
-mse = sklearn.metrics.mean_squared_error(y_test, y_pred)
-r2 = sklearn.metrics.r2_score(y_test, y_pred)
-
-print("MSE:", mse, "R2:", r2)
+print("Results of models without any feature engineered data:", results)
+noFEresults = pd.DataFrame.from_dict(results, orient='index')
+print(noFEresults)
 
 # Create polynomial features
 poly = PolynomialFeatures(degree=2, include_bias=False)
@@ -81,16 +100,9 @@ X_poly = poly.fit_transform(X)
 X_poly_scaled = scaler.fit_transform(X_poly)
 
 # Split the enhanced features into training and testing sets
-X_train_enhanced, X_test_enhanced, y_train, y_test = sklearn.model_selection.train_test_split(X_poly_scaled, y, test_size=0.2, random_state=42)
-
-# Define models to test
-models = {
-    "Linear Regression": LinearRegression(),
-    "KNN": KNeighborsRegressor(),
-    "Random Forest": RandomForestRegressor(random_state=42),
-    "Gradient Boosting": GradientBoostingRegressor(random_state=42),
-    "Ridge Regression": make_pipeline(StandardScaler(), Ridge(random_state=42))
-}
+X_train_enhanced, X_test_enhanced, y_train, y_test = sklearn.model_selection.train_test_split(X_poly_scaled, y,
+                                                                                              test_size=0.2,
+                                                                                              random_state=42)
 
 # Initialize dictionary to store results
 results = {}
@@ -108,11 +120,13 @@ for name, model in models.items():
     results[name] = {'MSE': mse, 'R2': r2}
 
 print("Results of Poly Scaled data:", results)
+polyScaleResults = pd.DataFrame.from_dict(results, orient='index')
+print(polyScaleResults)
 
 # Applying Reverse Feature Selection to remove the least important features
 # Initialize the model and RFE
 model = LinearRegression()
-rfe = RFE(model, n_features_to_select=5) # Selecting the top 10 features
+rfe = RFE(model, n_features_to_select=5)  # Selecting the top 6 features
 # Fit RFE
 rfe.fit(X_train, y_train)
 
@@ -122,6 +136,8 @@ print(selected_features)
 
 X_train_selected = X_train[selected_features]
 X_test_selected = X_test[selected_features]
+
+results = {}
 
 for name, model in models.items():
     # Train the model
@@ -135,5 +151,80 @@ for name, model in models.items():
     results[name] = {'MSE': mse, 'R2': r2}
 
 print("Results of RFE selected data:", results)
+RFEresults = pd.DataFrame.from_dict(results, orient='index')
+print(RFEresults)
+
+# Fine tune KNN hyper-parameters based on the RFE selected data as that model had the highest accuracy
+param_grid = {'n_neighbors': range(1, 10)}
+grid_search = GridSearchCV(KNeighborsRegressor(), param_grid, cv=5)
+grid_search.fit(X_train_selected, y_train)
+best_model = grid_search.best_estimator_
+# Predict on the testing set
+y_pred = best_model.predict(X_test_selected)
+# Calculate metrics
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+# Print results
+print('Results of fine-tuned KNN model : MSE:', mse, 'R2:', r2)
 
 
+# Transform the features
+X_scaled = scaler.fit_transform(X)
+
+# Split the enhanced features into training and testing sets
+X_train_enhanced, X_test_enhanced, y_train, y_test = sklearn.model_selection.train_test_split(X_scaled, y,
+                                                                                              test_size=0.2,
+                                                                                              random_state=42)
+
+# Initialize dictionary to store results
+results = {}
+
+# Train and evaluate each model
+for name, model in models.items():
+    # Train the model
+    model.fit(X_train_enhanced, y_train)
+    # Predict on the testing set
+    y_pred = model.predict(X_test_enhanced)
+    # Calculate metrics
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    # Store results
+    results[name] = {'MSE': mse, 'R2': r2}
+
+print("Results of Scaled data:", results)
+scaledResults = pd.DataFrame.from_dict(results, orient='index')
+print(scaledResults)
+
+# Based on the analysis scaled data performs the best when it comes to predicting Potential Impacts
+# Let's try the models with just Period, Impact Probability, Diameter and Velocity
+
+X_new = X[['Period','Impact.Probability..cumulative.','Vinfinity..km.s.','H..mag.']]
+X_new_scaled = scaler.fit_transform(X_new)
+
+# Split the enhanced features into training and testing sets
+X_train_enhanced, X_test_enhanced, y_train, y_test = sklearn.model_selection.train_test_split(X_new_scaled, y,
+                                                                                              test_size=0.2,
+                                                                                              random_state=42)
+
+
+# Initialize dictionary to store results
+results = {}
+
+# Train and evaluate each model
+for name, model in models.items():
+    # Train the model
+    model.fit(X_train_enhanced, y_train)
+    # Predict on the testing set
+    y_pred = model.predict(X_test_enhanced)
+    # Calculate metrics
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    # Store results
+    results[name] = {'MSE': mse, 'R2': r2}
+
+print("Results of Chosen Scaled data:", results)
+chosenScaledResults = pd.DataFrame.from_dict(results, orient='index')
+print(chosenScaledResults)
+
+
+# Bases on all the results, Random forest seem to perform the best along with the scaled data.
